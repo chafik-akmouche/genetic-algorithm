0889789c416ded00751aad8dd121caae1a5cf264
chafik-akmouche <chafik.akmouch@gmail.com>
2021-10-20 03:13:03 +0200

    initial commit

---
+++ src/kab/geneticalgorithm/main/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package kab.geneticalgorithm.main;
+
+/**
+ * @author chafik
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello !");
+	}
+
+}

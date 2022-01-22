26f6efe2252da7135d67f527f88d39bea9ecd0b6
chafik-akmouche <chafik.akmouch@gmail.com>
2022-01-22 13:15:20 +0100

    generation du graphique

--- src/kab/geneticalgorithm/utils/Curve.java
+++ src/kab/geneticalgorithm/utils/Curve.java
@@ -20,7 +20,7 @@ public class Curve {
 	 * @throws FileNotFoundException
 	 * @throws UnsupportedEncodingException
 	 */
-	public static void draw(int nb_iteration, ArrayList<Double> fitnessMin, ArrayList<Double> fitnessMoy, ArrayList<Double> fitnessMax) throws FileNotFoundException, UnsupportedEncodingException {
+	/*public static void draw(int nb_iteration, ArrayList<Double> fitnessMin, ArrayList<Double> fitnessMoy, ArrayList<Double> fitnessMax) throws FileNotFoundException, UnsupportedEncodingException {
 		int afficherCourbe = JOptionPane.showOptionDialog(
 				null, "Voulez-vous visualiser la courbe Fitness/Iteration ?", "Courbe Fitness",
 	            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
@@ -42,14 +42,74 @@ public class Curve {
 		} else {
 			System.exit(0);
 		}
+	}*/
+	/**
+	 * création du script générant les graphes + test du système d'exploitation
+	 * @param nb_generation
+	 * @param fitnessMin
+	 * @param fitnessMoy
+	 * @param fitnessMax
+	 * @throws FileNotFoundException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void draw2(int nb_generation, ArrayList<Double> fitnessMoy, ArrayList<Integer> historiqueOp) throws FileNotFoundException, UnsupportedEncodingException {
+		int afficherCourbe = JOptionPane.showOptionDialog(
+				null, "Voulez-vous visualiser le graphique ?", "Courbe Fitness",
+	            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+		if (afficherCourbe == 0) {
+			String SE = System.getProperty("os.name").toLowerCase();
+			if (SE.indexOf("nux") >= 0) {
+				PrintWriter writer = new PrintWriter("data2.dat", "UTF-8");
+				writer.println("Generation F-Moy(op1) F-Moy(op2) F-Moy(op3) F-Moy(op4)");
+				for (int i=0; i<nb_generation; i++) {
+					writer.print(i+" ");
+					for (int j=0; j<historiqueOp.size(); j++) {
+						if(historiqueOp.get(j) == 0) {
+							writer.print(0+" ");
+						} else {
+							writer.print( fitnessMoy.get(i) / historiqueOp.get(j) + " ");
+						}
+					}
+					writer.print("\n");
+				}
+				writer.close();
+				Curve.shellExec2();
+			} else {
+				JOptionPane.showMessageDialog(new JFrame(), "Vous êtes sur "+SE+".\nNous ne pouvons malheureusement pas générer la courbe.", "Erreur - "+SE,
+				        JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+		} else {
+			System.exit(0);
+		}
 	}
+	
 	/**
 	 * execution du script
 	 */
-	public static void shellExec () {
+	/*public static void shellExec () {
 		Process process = null;
 		try {
 			process = Runtime.getRuntime().exec("sh script.sh");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			process.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			process.destroy();
+		}
+	}*/
+	
+	/**
+	 * execution du script
+	 */
+	public static void shellExec2() {
+		Process process = null;
+		try {
+			process = Runtime.getRuntime().exec("sh script2.sh");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/kab/geneticalgorithm/utils/PM.java
+++ src/kab/geneticalgorithm/utils/PM.java
@@ -1,4 +1,6 @@
 package kab.geneticalgorithm.utils;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 /**
  * Sélection avec probability matching (PM)
@@ -35,4 +37,135 @@ public class PM {
 		}
 		return i;
 	}	
+	
+	public static void launche(ArrayList<Integer> historiqueOp, ArrayList<Double> historiqueFit) throws FileNotFoundException, UnsupportedEncodingException {
+		/** liste des opérateurs */
+		ArrayList <Operator> listeOp = new ArrayList <Operator>();
+		/** sauvegarde des fitness */
+		ArrayList <Double> fitnessMin = new ArrayList <Double>();
+		ArrayList <Double> fitnessMoy = new ArrayList <Double>();
+		ArrayList <Double> fitnessMax = new ArrayList <Double>();
+		
+		//initialisation individus & population
+		Population pop = new Population(Main.TAILLE_POPULATION, Main.TAILLE_INDIVIDU).initialiationPopulation();
+		System.out.println("### Population initiale");
+		pop.affichagePpulation(pop);	
+		
+		//initialisation de la liste des opérateurs de mutation
+		for (int i=0; i<Main.MutationOperators.length; i++) {
+			listeOp.add(new Operator(Main.MutationOperators[i], Main.pInit, 0));
+		}
+		//affichage de l'etat initial des opérateurs
+		affichageEtatOp(listeOp);
+		GeneticAlgorithm GA = new GeneticAlgorithm();
+		
+		//init des listes fitness
+		fitnessMin.add(0.0);	fitnessMoy.add(0.0);	fitnessMax.add(0.0);
+				
+		int generation = 1;
+		boolean popParfaite = false;
+		while (generation <= Main.MAX_GENERATION && !popParfaite) {
+			System.out.println("############### GENERATION (" + generation + ") ###############");
+			//sélectionner un operateur
+			int index = PM.selectOperateur(listeOp);
+			String current_op = listeOp.get(index).getName();
+			listeOp.get(index).incNb_fois();
+			listeOp.get(index).addToGenerations(generation);
+			
+			//mutation
+			pop = GA.mutationPopulation(pop, current_op);
+			
+			//croisement & selection & remplacement
+			pop = GA.croisementMonoPoint(pop, Main.S2M, Main.R2MA);	
+			//pop = GA.croisementSimplePopulation(pop, S2M, R2MA);
+			//pop = GA.croisementUniformePopulation(pop, S2M, R2MA);
+			
+			// Trier les individus par ordre croissant des fitness
+			pop.TrierIndividusParFitness();		
+			//calcul de la somme des fitness des individus
+			double sommeFitnessIndividus = 0;
+			for (int x=0; x<Main.TAILLE_POPULATION; x++) {
+				sommeFitnessIndividus = sommeFitnessIndividus + (pop.getIndividus()[x].getFitness());
+			}
+			//ajout des fitness
+			fitnessMin.add((double) (((pop.getIndividus()[Main.TAILLE_POPULATION-1].getFitness())*100)/Main.TAILLE_INDIVIDU));
+			fitnessMoy.add(((sommeFitnessIndividus/Main.TAILLE_POPULATION)*100)/Main.TAILLE_INDIVIDU);
+			fitnessMax.add((double) (((pop.getIndividus()[0].getFitness())*100)/Main.TAILLE_INDIVIDU));
+					
+			pop.affichagePpulation(pop);
+			affichageFitness(generation, fitnessMin, fitnessMoy, fitnessMax);
+			System.out.println("### Opérateur sélectionné : " + current_op);
+			
+			//ajout de la récompense
+			Reward.majReward(listeOp.get(index), (int)(fitnessMoy.get(generation-1)*Main.TAILLE_INDIVIDU)/100, (int)(fitnessMoy.get(generation)*Main.TAILLE_INDIVIDU)/100);
+			
+			//ajout de l'amélioration
+			Reward.majAmelioration(listeOp.get(index), (double)(fitnessMoy.get(generation-1)), (double)(fitnessMoy.get(generation)));
+			
+			//mise à jour de la roulette proportionnelle
+			for(int i=0; i<listeOp.size(); i++) {
+				PM.majRoulette(listeOp.get(i), listeOp.size());
+			}
+			
+			//affichage de l'etat actuel des opérateurs
+			affichageEtatOp(listeOp);
+			
+			// Condition d'arret
+			if ((sommeFitnessIndividus / Main.TAILLE_POPULATION) == Main.TAILLE_INDIVIDU) {
+				popParfaite = true;
+			}
+//			if (generation == 1) {
+//				for(int i=0; i<Main.MAX_GENERATION; i++) {
+//					historiqueFit.add(fitnessMoy.get(generation));
+//				}
+//			} else if (generation > 1) {
+//				for(int i=0; i<historiqueFit.size(); i++) {
+//					historiqueFit.set(i, (fitnessMoy.get(generation) + historiqueFit.get(i))/2);
+//				}
+//			}
+			generation++;
+		}
+		for(int i=0; i<listeOp.size(); i++) {
+			historiqueOp.set(i, (listeOp.get(i).getNb_fois())+historiqueOp.get(i));
+		}
+		
+		for(int i=0; i<fitnessMax.size(); i++) {
+			historiqueFit.set(i, (fitnessMax.get(i) + historiqueFit.get(i)));
+		}
+		//affichage de la courbe
+		//Curve.draw(generation, fitnessMin, fitnessMoy, fitnessMax);
+	}
+	
+	/**
+	 * affichage de l'etat des opérateurs
+	 */
+	public static void affichageEtatOp (ArrayList<Operator> listeOp) {
+		System.out.println("### ETAT DES OPERATEURS");
+		for (int i=0; i<listeOp.size(); i++) {
+			System.out.print("op "+i+": "+listeOp.get(i).getName()+" | proba="+listeOp.get(i).getProba()+" | nb_fois="+listeOp.get(i).getNb_fois());
+			System.out.print("\n\tReward: [ ");
+			for(int j=0; j<listeOp.get(i).getListeReward().size(); j++) {
+				System.out.print(listeOp.get(i).getListeReward().get(j)+" ");
+			}
+			System.out.print("]\n\tGénération: [ ");
+			for(int k=0; k<listeOp.get(i).getGenerations().size(); k++) {
+				System.out.print(listeOp.get(i).getGenerations().get(k)+" ");
+			}
+			System.out.print("]\n\tAmélioration: [ ");
+			for(int k=0; k<listeOp.get(i).getListeAmelioration().size(); k++) {
+				System.out.print(listeOp.get(i).getListeAmelioration().get(k)+" ");
+			}
+			System.out.print("]\n");
+		}
+	}
+	
+	/**
+	 * affichage generation & fitness (%)
+	 * @param index
+	 */
+	public static void affichageFitness (int index, ArrayList<Double>fitnessMin, ArrayList<Double>fitnessMoy, ArrayList<Double>fitnessMax) {
+		System.out.println("Fitness Min : "+ fitnessMin.get(index)+" %\t| "+
+							"Fitness Moy : "+ fitnessMoy.get(index)+" %\t| "+
+							"Fitness Max : "+ fitnessMax.get(index)+" %");
+	}
 }
